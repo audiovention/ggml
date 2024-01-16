16f30198ea22b51efdb986a637a974d5b9082925
Momchil Jeliazkov <jeliazkov.momchil@gmail.com>
2024-01-16 19:02:30 +0200

    get rid of framework files, get rid of shader source file - shader is now inline in ggml-wgpu.c

--- src/ggml-wgpu.c
+++ src/ggml-wgpu.c
@@ -2,11 +2,13 @@
 #include "ggml.h"
 
 #include <webgpu/webgpu.h>
-#include "framework.h"
+#include <webgpu/wgpu.h>
 
 #include <stdarg.h>
+#include <stdio.h>
 
 #define MIN_STORAGE_BUFFER_ALIGNMENT 256
+#define UNUSED(x) (void)(x)
 
 #define ASSERT_CHECK(x) \
     if (!(x)) { \
@@ -18,22 +20,86 @@
 static void handle_request_adapter(WGPURequestAdapterStatus status,
                                    WGPUAdapter adapter, char const *message,
                                    void *userdata) {
-  UNUSED(status)
-  UNUSED(message)
+  UNUSED(status);
+  UNUSED(message);
   *(WGPUAdapter *)userdata = adapter;
 }
 static void handle_request_device(WGPURequestDeviceStatus status,
                                   WGPUDevice device, char const *message,
                                   void *userdata) {
-  UNUSED(status)
-  UNUSED(message)
+  UNUSED(status);
+  UNUSED(message);
   *(WGPUDevice *)userdata = device;
 }
 static void handle_buffer_map(WGPUBufferMapAsyncStatus status, void *userdata) {
-  UNUSED(userdata)
+  UNUSED(userdata);
   printf(LOG_PREFIX " buffer_map status=%#.8x\n", status);
 }
 
+#define MULTILINE(...) #__VA_ARGS__
+static const char* shader_src = MULTILINE(
+struct TensorDimensionParams {
+        ne00 : i32,
+        ne01 : i32,
+        ne02 : i32,
+        ne03 : i32,
+
+        nb00 : u32,
+        nb01 : u32,
+        nb02 : u32,
+        nb03 : u32,
+
+        ne10 : i32,
+        ne11 : i32,
+        ne12 : i32,
+        ne13 : i32,
+
+        nb10 : u32,
+        nb11 : u32,
+        nb12 : u32,
+        nb13 : u32,
+
+        ne0 : i32,
+        ne1 : i32,
+        ne2 : i32,
+        ne3 : i32,
+
+        nb0 : u32,
+        nb1 : u32,
+        nb2 : u32,
+        nb3 : u32,
+
+        offs_src0 : u32,
+        offs_src1 : u32,
+        offs_dst  : u32,
+}
+
+
+
+@group(0) @binding(0)
+var<storage,read_write> src0: array<f32>;
+
+@group(0) @binding(1)
+var<storage,read_write> src1: array<f32>;
+
+@group(0) @binding(2)
+var<storage,read_write> dst: array<f32>;
+
+@group(0) @binding(3)
+var<uniform> tensor_dimension_params: TensorDimensionParams;
+
+
+
+@compute
+@workgroup_size(1)
+fn test_kernel(@builtin(global_invocation_id) global_id: vec3<u32>) {
+    let x = src0[global_id.x + tensor_dimension_params.offs_src0/4u];
+    dst[global_id.x + tensor_dimension_params.offs_dst/4u] = x / (1.0 + exp(-x)); ;
+}
+
+
+);
+#undef MULTILINE
 
 #undef MIN
 #undef MAX
@@ -50,7 +116,6 @@ static void handle_buffer_map(WGPUBufferMapAsyncStatus status, void *userdata) {
 #define GGML_WGPU_LOG_ERROR(...) ggml_wgpu_log(GGML_LOG_LEVEL_ERROR, __VA_ARGS__)
 #endif
 
-#define UNUSED(x) (void)(x)
 
 struct ggml_wgpu_buffer {
     const char * name;
@@ -202,7 +267,20 @@ struct ggml_wgpu_context * ggml_wgpu_init() {
     ctx->n_buffers = 0;
 
     // load library
-    ctx->shader_module = frmwrk_load_shader_module(ctx->device, "ggml-wgpu.wgsl");
+    ctx->shader_module = wgpuDeviceCreateShaderModule(
+      ctx->device, &(const WGPUShaderModuleDescriptor){
+                  .label = "ggml_shader_module",
+                  .nextInChain =
+                      (const WGPUChainedStruct *)&(
+                          const WGPUShaderModuleWGSLDescriptor){
+                          .chain =
+                              (const WGPUChainedStruct){
+                                  .sType = WGPUSType_ShaderModuleWGSLDescriptor,
+                              },
+                          .code = shader_src,
+                      },
+              });
+
     ASSERT_CHECK(ctx->shader_module);
 
     WGPUBindGroupLayoutEntry bindGroupLayoutEntries[4] = {0};
