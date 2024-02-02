1d5a6257dc71fbf89e54fe1a206550fcb2de875b
Momchil Jeliazkov <jeliazkov.momchil@gmail.com>
2024-02-02 16:43:50 +0200

    fix the uniform indexing issue

--- src/ggml-wgpu.c
+++ src/ggml-wgpu.c
@@ -447,8 +447,9 @@ fn kernel_conv_1d_small_kern(@builtin(global_invocation_id) global_id: vec3<u32>
     var output : f32 = 0.0;
 
     if (has_bias) {
-        // let bias = extra_uniform1[global_id.y/4u][global_id.y%4u];
-        let bias = get_src2(0u, global_id.y, 0u);
+        let bias_idx = global_id.y * tensor_dimension_params.src[2].nb[1];
+        let bias = extra_uniform1[bias_idx/4u][bias_idx%4u];
+        // let bias = get_src2(0u, global_id.y, 0u);
         output += bias;
     }
 
@@ -463,9 +464,9 @@ fn kernel_conv_1d_small_kern(@builtin(global_invocation_id) global_id: vec3<u32>
         let kernel_base_idx = global_id.y + ik * tensor_dimension_params.src[0].nb[2];
         for (var ic = 0u; ic < input_channels; ic = ic + 1u) {
             let input = get_src1_lin(in_idx_offset + ic * tensor_dimension_params.src[1].nb[1]);
+            // let kernel = get_src0(global_id.y, ic, ik);
             let kernel_idx = kernel_base_idx + ic * tensor_dimension_params.src[0].nb[1];
-            let kernel = get_src0(global_id.y, ic, ik);
-            // let kernel = extra_uniform0[kernel_idx/4u][kernel_idx%4u];
+            let kernel = extra_uniform0[kernel_idx/4u][kernel_idx%4u];
             output = output + input * kernel;
         }
     }
