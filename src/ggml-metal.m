0b57a8c3d17e6d5541b8e1a5357a9ba15aaa6788
Momchil Jeliazkov <jeliazkov.momchil@gmail.com>
2024-04-09 15:33:23 +0300

    run the simdgroup matrices through local mem, in order to zero out values that are out of bounds

--- src/ggml-metal.m
+++ src/ggml-metal.m
@@ -1106,9 +1106,13 @@ void ggml_metal_graph_compute(
 #else
 #if 1
                             if ([ctx->device supportsFamily:MTLGPUFamilyApple7] && (8 == input_channels || 16 == input_channels) && (8 == output_channels || 16 == output_channels)) {
+                                int threadGroupMemMult = output_channels;
+                                if (input_channels > threadGroupMemMult) {
+                                    threadGroupMemMult = input_channels;
+                                }
                                 dispatch_y = 1;
                                 GGML_METAL_SET_F32_OR_F16_PIPELINE(conv_1d_small_kern_nx8kx8m_simdgr)
-                                [encoder setThreadgroupMemoryLength:output_channels*threadgroupSize*ggml_element_size(dst) atIndex:0];
+                                [encoder setThreadgroupMemoryLength:threadGroupMemMult*threadgroupSize*ggml_element_size(dst) atIndex:0];
                             } else
 #endif
                             if (1 == nk) {
